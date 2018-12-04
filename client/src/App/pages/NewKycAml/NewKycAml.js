ff28523b1d15fc76d336fb417b9011f911ba8bd9
topdeveloper2019 <topdeveloper2019@outlook.com>
2018-12-04 23:25:40 +0800

    update front-end kyc/aml page

--- client/src/App/pages/NewKycAml/NewKycAml.js
+++ client/src/App/pages/NewKycAml/NewKycAml.js
@@ -6,15 +6,7 @@ import SideBar   from '../../components/SideBar';
 import NavBar    from '../../components/NavBar';
 import CreateDialog from  '../../components/Campaign/CreateDialog';
 import ManageDialog from  '../../components/Campaign/ManageDialog';
-import campaign_eye from "./campaign_eye.svg"
-import pen from "./pen.svg"
-import development from "./development.svg"
-
-import campaignImg from "./campaign2.png"
-import questionMarkPurple from "./Layer 3 copy.svg"
-import dummy from "./dummy.svg"
-import people from "./people.svg"
-import users from "./users.svg"
+import TokenList from '../../components/Token/List';
 
 const FundingItem = React.memo(({current, target}) => (
   <div className='col text-left'>
@@ -55,51 +47,38 @@ class Content extends React.PureComponent {
     const { tokens } = this.props
     return (
       <div className='content-wrapper'>
-        <div className="row justify-content-between" style={{marginTop: '50px'}}>
-          <div className="float-right">
-            <button className="create-btn-campaign" onClick={this.openCreateDialog.bind(this)}>Create Campaign</button>
+        <div className='justify-content-between tokens-wrapper'>
+          <div className='section-header'>
+            <div className='float-left'>
+              <h2>My Tokens</h2>
+            </div>
+            <div className='float-right'>
+              <button className='create-btn' onClick={this.openCreateDialog.bind(this)}>+ Create Token</button>
+            </div>
           </div>
+          <div className='section-content'>
+            <TokenList tokens = {tokens} onClickItem={this.openManageDialog.bind(this)}/>
+          </div>
+          <Modal isOpen={this.state.showCreateModal} style={{content:{border:'0 none', background:'none', top: '80px', left: '180px', bottom: '80px', right: '180px'}}}>
+            <CreateDialog onClose={this.closeCreateDialog.bind(this)}/>
+          </Modal>
+          <Modal isOpen={this.state.showManageModal} style={{content:{border:'0 none', background:'none', top: '80px', left: '180px', bottom: '80px', right: '180px'}}}>
+            <ManageDialog onClose={this.closeManageDialog.bind(this)}/>
+          </Modal>
         </div>
-        <div className='row justify-content-between campaign-wrapper'>
-            <div className='col-12 col-xl-6 campaign-section'>
-              <div className='clearfix campaign-section-header'>
-                <span className='float-left'>EXAMPLE CAMPAIGN</span>
-                <span className='float-right'>
-                  <img src={campaign_eye} className='ml-3'  style={{height: '22px', width: '16px' }}/>
-                  <img src={pen} className='ml-3' style={{height: '22px', width: '16px' }}/>
-                  <img src={development} className='ml-3'  style={{height: '22px', width: '16px' }}/>
-                </span>
-              </div>
-
-              <div className='clearfix campaign-description'>
-                <span className='float-left'>Description of Campaign</span>
-              </div>
-        
-              <div className='clearfix campaign-eth-text'>
-                <span className='float-left font-weight-bold' style={{fontSize: '7vmin'}}>ETH 237,650</span>
-              </div>
-
-              <div className='clearfix text-left'>
-                <span style={{color: '#666674', fontFamily: 'SF Pro Text', fontSize: '18px'}}>Raised</span>
-
-                <div class="progress" style={{ height: '7px', borderRadius: '2px', marginBottom: '37px', marginTop: '15px', marginRight: '2px' }}>
-                  <div class="progress-bar" role="progressbar" style={{ width: '75%', backgroundImage: "linear-gradient(90deg, #ef4d5b 0%, #fe687b 100%)" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                  </div>
-                </div>
-
-              </div>
-
-              <div className='row'>
-                <FundingItem current='60 ETH' target='Funding goal' />
-                <FundingItem current='6 ETH' target='Fund Raised' />
-                <FundingItem current='54' target='Days to go' />
-              </div>
+        <div className='justify-content-between tokens-wrapper'>
+          <div className='section-header'>
+            <div className='float-left'>
+              <h2>My Tokens</h2>
             </div>
-
-            <div className='col-12 col-xl-6 px-0' >
-              <img src={campaignImg} className='img-fluid w-100  h-100' />
+            <div className='float-right'>
+              <button className='create-btn' onClick={this.openCreateDialog.bind(this)}>+ Create Token</button>
             </div>
-            <Modal isOpen={this.state.showCreateModal} style={{content:{border:'0 none', background:'none', top: '80px', left: '180px', bottom: '80px', right: '180px'}}}>
+          </div>
+          <div className='section-content'>
+            <TokenList tokens = {tokens} onClickItem={this.openManageDialog.bind(this)}/>
+          </div>
+          <Modal isOpen={this.state.showCreateModal} style={{content:{border:'0 none', background:'none', top: '80px', left: '180px', bottom: '80px', right: '180px'}}}>
             <CreateDialog onClose={this.closeCreateDialog.bind(this)}/>
           </Modal>
           <Modal isOpen={this.state.showManageModal} style={{content:{border:'0 none', background:'none', top: '80px', left: '180px', bottom: '80px', right: '180px'}}}>
